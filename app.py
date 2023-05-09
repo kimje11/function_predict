fe4ccfef27900b8edfbfe53324403b38085a2f34
kimje11 <81818797+kimje11@users.noreply.github.com>
2023-05-10 03:21:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,9 @@ def dynamic_slider(min_val, max_val,name):
 # font_prop = fm.FontProperties(fname=font_path, size=12)
 
 # plt.rc('font', family=font_prop.get_name())
-import matplotlib.font_manager as form
-font_list = [font.name for font in form.fontManager.ttflist]
-for f in font_list:
-    print(f)
+from matplotlib import rc
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 
 #초기 리스트 정리
 title_count = st.session_state.get("title_count", 0)
